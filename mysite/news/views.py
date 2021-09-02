dfdd89f31eb57ce9c0b10ffe5f54d08da0a25692
nio <bhf@gjhgj.com>
2021-09-02 13:37:03 +0300

    add http-response "Hello test page"

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print(request)
     return HttpResponse("Hello index")
+
+def test(request):
+    return HttpResponse("<h1>Hello test page</h1>")
